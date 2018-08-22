ac8a597d56d13856ddce6f4e20ca970fa9cac2d9
Mayank <mayankgautam98@gmail.com>
2018-08-22 16:15:57 +0530

    Added A Line Of Code To Properly Clear The GUI Window

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@ void init()
 	glutInitWindowSize(800,600);
 	glutCreateWindow("OneBigSux");
 	glClearColor(0.9,0.9,0.9,0); //setting background to black
+	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(0.8,0.0,0.0); //drawings will have red shade
 	gluOrtho2D(-399,400,-299,300); //defined limits in which to draw
 }
